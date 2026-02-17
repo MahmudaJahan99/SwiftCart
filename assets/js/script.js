9a323a5104813394387fffec490d2454b113d647
DepressedPotatok <mj.mjahan045@gmail.com>
2026-02-17 13:02:38 +0600

    trending section

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+//
+const productsDisplayContainer = document.getElementById(
+  "products-display-container",
+);
+
+// ==================== LOAD ALL DATA ====================
+const loadAllProducts = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((response) => response.json())
+    .then((products) => {
+      const trending = products
+        .sort(
+          (product1, product2) => product2.rating.rate - product1.rating.rate,
+        )
+        .slice(0, 3);
+      displayAllProducts(trending);
+    });
+};
+
+// ==================== DISPLAY ALL DATA ====================
+const displayAllProducts = (products) => {
+  // clear the container
+  productsDisplayContainer.innerHTML = "";
+
+  // create dynamic cards for products
+  for (let product of products) {
+    const cardContainer = document.createElement("div");
+    cardContainer.innerHTML = `
+        <!-- Card ${product.id} -->
+          <div class="card bg-base-100 w-11/12 m-auto shadow-sm">
+            <figure>
+              <img
+                src="${product.image}"
+                alt="Product Image"
+              />
+            </figure>
+            <div class="card-body py-10">
+              <div class="flex justify-between items-center">
+                <span class="badge badge-xl">${product.category}</span>
+                <span class="text-2xl text-gray-600">
+                  <i class="ri-star-fill"></i>
+                  ${product.rating.rate} (${product.rating.count})
+                </span>
+              </div>
+              <h4 class="card-title text-2xl my-5 font-medium">
+                ${product.title}
+              </h4>
+              <p class="card-title text-3xl">$${product.price}</p>
+              <div class="card-actions justify-between mt-5 gap-5">
+                <button class="btn btn-primary w-full lg:w-[40%]">
+                  <i class="ri-eye-line"></i> Details
+                </button>
+                <button class="btn btn-primary w-full lg:w-[40%]">
+                  <i class="ri-shopping-cart-line"></i> Buy
+                </button>
+              </div>
+            </div>
+          </div>
+    `;
+
+    // append container
+    productsDisplayContainer.append(cardContainer);
+  }
+};
+
+// ==================== CALL LOAD ALL DATA ====================
+loadAllProducts();
